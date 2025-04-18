77d3f1f50743f1764faacc5bf7929d91fb29d3ab
amirparsaro <amirparsarouhani@gmail.com>
2025-04-18 11:53:20 +0330

    create class RegularBehavior

---
+++ src/player/RegularBehavior.java
@@ -0,0 +1,25 @@
+package player;
+
+public class RegularBehavior implements UserBehavior {
+    private int playingLimit = 5;
+
+    @Override
+    public void createPlaylist(String Title, User Owner) {
+        throw new InvalidOperationException("Regular user Cannot create a playlist.");
+    }
+
+    @Override
+    public void playMusic(Music music) {
+        if (playingLimit > 0) {
+            Music.play();
+            playingLimit--;
+        } else {
+            throw new InvalidOperationException("Music playing limit reached for user.");
+        }
+    }
+
+    @Override
+    public void buyPremium(User owner, int month) {
+        // to do after creating field behavior in User.
+    }
+}
